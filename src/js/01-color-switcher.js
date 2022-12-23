099fbb9c43f7cb93eae446d1e03d01decd73c0ff
alexveryk <veryk.av@gmail.com>
2022-12-23 23:46:17 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+btnStart.addEventListener('click', selectBtnStart);
+btnStop.addEventListener('click', selectBtnStop);
+
+let intervalId = null;
+btnStop.setAttribute('disabled', true);
+
+function selectBtnStart(event) {
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  console.log('Entered Start');
+
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+
+  intervalId = setInterval(() => {
+    console.log(getRandomHexColor());
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function selectBtnStop(event) {
+  console.log('Entered Stop');
+  clearInterval(intervalId);
+
+  btnStop.setAttribute('disabled', true);
+  btnStart.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
